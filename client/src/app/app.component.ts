e3a10d73ec7875a4f9a061d684db978b57ec817b
hamid <hamidreza.mohammadian@hotmail.com>
2022-11-28 11:01:32 +0330

    add auth with token

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  title = 'Dating App';
+
+  constructor(private http: HttpClient){}
+
+  users: any;
+
+  ngOnInit() {
+    this.getUsers()
+  }
+
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (res) => {
+        this.users = res
+      },
+
+      error: (err) => {
+        console.log(err)
+      }
+    })
+  }
+
+
+
+}
